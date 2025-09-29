7fd08967983b4c1bd6c8bc70e8523ef2ef6ed023
dani23 <giacche.dani@gmail.com>
2025-09-29 22:27:39 +0200

    Continue Texture class

--- include/graphics/textures/Texture.cpp
+++ include/graphics/textures/Texture.cpp
@@ -1,13 +1,34 @@
 #include "Texture.h"
 
-Texture::Texture(std::string texture_path, unsigned int texture_type = GL_TEXTURE_2D){
-    data = stbi_load(texture_path.c_str(), &width, &height, &nrChannels, 0); 
+Texture::Texture(std::string texture_path, unsigned int texture_type = GL_TEXTURE_2D)
+    : texture_path(texture_path)
+{
+    glGenTextures(1, &this->id);
+    this->LoadTexture();
+    this->Bind(texture_type)
 }
 
-void Texture::GenerateTexture(){
+void Texture::Bind(unsigned int texture_type = GL_TEXTURE_2D)
+{
+    glBindTexture(GL_TEXTURE_2D, ID());
+}
+
+std::expected<bool, std::string> Texture::LoadTexture()
+{
+    unsigned char *data = stbi_load(texture_path.c_str(), &width, &height, &nrChannels, 0);
+    if (!data)
+    {
+        std::unexpected("Failed to load texture");
+        return false;
+    }
+    this->data = data;
+    return true;
+}
+
+void Texture::GenerateTexture()
+{
     glGenTextures(1, &this->id);
     this->Bind();
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this->height, 0, GL_RGB, GL_UNSIGNED_BYTE, this->data);
     glGenerateMipmap(GL_TEXTURE_2D);
-    stbi_image_free(data);
 }
--- include/graphics/textures/Texture.h
+++ include/graphics/textures/Texture.h
@@ -3,20 +3,24 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 #include <string>
+#include <expected>
 
 class Texture{
     public:
         Texture(std::string texture_path, unsigned int texture_type = GL_TEXTURE_2D);
+        
         int Width() const { return width; };
         int Height() const { return height; };
         int NrChannels() const { return nrChannels; };
         unsigned int ID() const { return id; };
 
-        void Bind() const { glBindTexture(GL_TEXTURE_2D, ID()); };
+        void Bind(unsigned int texture_type = GL_TEXTURE_2D);
         void GenerateTexture();
     private:
+        std::expected<bool, std::string> LoadTexture();
         int width, height, nrChannels;
         unsigned char *data;
         unsigned int id;
         unsigned int type;
+        std::string texture_path;
 };
